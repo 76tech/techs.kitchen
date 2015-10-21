947829bdb643b04b71c8192d125eb8794559e99f
Ben Snyder <76tech@gmail.com>
2015-10-21 09:13:24 -0400

    initial checkin

---
+++ site/js/master.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+});
+
+$(document).on('click touch','#devicesButton',function(e){
+    e.preventDefault();
+});
+
+function loadCategoryView() {
+
+}
+
+function loadCuisineView() {
+
+}
+
+function loadSeasonView() {
+
+}
