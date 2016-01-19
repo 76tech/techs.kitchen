e37d2b01ff8e3781fd278acba3ed8a4396f6c00f
Ben Snyder <ben.snyder@philips.com>
2016-01-19 16:43:15 -0500

    rough WIP back end

--- back-end/app/models/Recipe.js
+++ back-end/app/models/Recipe.js
@@ -1,3 +1,4 @@
+var _ = require('lodash');
 /*
 var Recipe = {
   id: '',
@@ -10,6 +11,39 @@ var Recipe = {
   collection: recipes
 
 */
+
+
+var testRecipes = [
+{
+  'id':1,
+  'name':'Puttanesca Sauce',
+  'description':'Bold, spicy tomato based pasta sauce',
+  'equipment':['Saucepan', 'Wooden spoon'],
+  'ingredients':['1/3 cup olive oil', '5 cloves garlic', '2 tsp anchovy paste', '1/2 tsp red pepper flakes',
+                 '1 tsp salt', '1 28 oz can crushed tomatoes', '1/2 cup Kalamata olives, pitted and chopped',
+                 '2 tbsp capers, drained', 'Sugar, 1 pinch', '3/4 cup coarsely chopped basil'
+               ],
+  'method':['Saute garlic, anchovy paste, red pepper flakes, and salt in olive oil until garlic is tender',
+            'Stir in tomatoes, olives, capers, sugar.  Simmer for 30 minutes.',
+            'Toss with pasta, sprinkle with basil.'],
+  'categories':[{'name':'cuisine','value':'Italian'},
+                {'name':'meal','value':'dinner'},
+                {'name':'course','value':'main'}]
+},
+{
+  'id':2,
+  'name':'Spaghetti (boxed)',
+  'description':'Just boxed spaghetti',
+  'ingredients':['Spaghetti, 1 box', 'Water', 'Salt'],
+  'method':['Salt water in large pot.', 'Bring salted water to rolling boil',
+            'Add pasta to boiling water while stirring to prevent sticking',
+            'When water returns to boil, cook according to package directions, stirring occassionally'],
+  'categories':[{'name':'cuisine','value':'Italian'},
+                {'name':'meal','value':'dinner'},
+                {'name':'course','value':'main'}]
+}
+];
+
 function saveRecipe(callback) {
   callback();
 }
@@ -18,15 +52,53 @@ function deleteRecipe(callback) {
   callback();
 }
 
-function find(callback) {
+// find all recipes
+function findRecipes(callback) {
   var err = null;
-  callback(err, [{'name':'water'}, {'name':'ice'}]);
+  callback(err, testRecipes);
 }
 
-function findOne(id,callback) {
+// return one recipe by ID
+function findOneRecipe(id,callback) {
   var err = null;
-  callback(err, [{'id':id,'name':'water'}]);
+  var recipe = _.find(testRecipes, function(r) { return r.id == id; } );
+  callback(err, recipe);
 }
+
+// returns all categories and their values
+function getCategoriesAndValues(callback) {
+  var err = null;
+  callback(null, getAllCategoryValues('meal'));
+}
+
+function getCategoryValues(categoryName,callack) {
+  //get value where recipe.categories.name=categoryName;
+
+  var err = null;
+  callback(err,_.union(_.map(_.filter(_.union.apply(_, _.map(testRecipes, 'categories')), {'name':categoryName}),'value')));
+}
+//-----------------------------------
+function getListOfRecipeIds() {
+  return Object.keys(testRecipes);
+}
+
+function getRecipeCategoryNames(recipeId) {
+  var recipe = testRecipes[recipeId];
+  return _.map(recipe.categories, 'name');
+}
+
+function getRecipeCategories(recipeId) {
+  var recipe = testRecipes[recipeId];
+  return recipe.categories;
+}
+
+// returns all categories and their values
+function getCategories(callback) {
+  var err = null;
+  callback(null, _.union.apply(_, getListOfRecipeIds().map(getRecipeCategoryNames) ) );
+}
+
+
 
 function findByCategory(category,value,callback) {
   var err = null;
@@ -38,6 +110,8 @@ function findByCategory(category,value,callback) {
 //module.exports = Recipe;
 module.exports.saveRecipe = saveRecipe;
 module.exports.deleteRecipe = deleteRecipe;
-module.exports.find = find;
-module.exports.findOne = findOne;
+module.exports.findRecipes = findRecipes;
+module.exports.findOneRecipe = findOneRecipe;
+module.exports.getCategoriesAndValues = getCategoriesAndValues;
+module.exports.getCategoryValues = getCategoryValues;
 module.exports.findByCategory = findByCategory;
--- back-end/server.js
+++ back-end/server.js
@@ -6,10 +6,12 @@ var restify = require('restify');
 var server = restify.createServer();
 var port = process.env.PORT || 8080;
 
+// Server setup/config
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.jsonp());
 server.use(restify.bodyParser({ mapParams: true }));
 
+// Generic requests/blank routes
 server.get('/', function(req, res) {
   res.json({message: 'Welcome to tkapi'});
 });
@@ -17,27 +19,51 @@ server.post('/', function(req, res) {
   res.json({message: 'Welcome to tkapi'});
 });
 
-server.post('/recipes', function(req,res) {
-  console.log(req.params.name);
-  res.json({message: 'Recipe created'});//, body: JSON.stringify(req.body)});
-});
-
+// Recipe handling
 server.get('/recipes', function(req,res) {
-  Recipe.find(function(err,recipes) {
+  Recipe.findRecipes(function(err,recipes) {
     if(err)
       res.send(err);
     res.json(recipes);
   });
 });
 
+// create new recipe
+server.post('/recipes', function(req,res) {
+  console.log(req.params.name);
+  res.json({message: 'Recipe created'});//, body: JSON.stringify(req.body)});
+});
+
+// Recipes by ID handling
 server.get('/recipes/:id', function(req,res) {
-  Recipe.findOne(req.params.id, function(err,recipe) {
+  Recipe.findOneRecipe(req.params.id, function(err,recipe) {
     if(err)
       res.send(err);
     res.json(recipe);
   });
 });
 
+// Category handling
+// gets all categories - returns array of categories and all unique values
+server.get('/categories', function(req,res) {
+  Recipe.getCategoriesAndValues(function(err,categories) {
+    if(err)
+      res.send(err);
+    res.json(categories);
+  });
+});
+
+// gets all values for category
+server.get('/categories/:category', function(req,res) {
+  Recipe.getCategoryValues(req.params.category, function(err,categoryValues) {
+    if(err)
+      res.send(err);
+    res.json(categoryValues);
+  });
+});
+
+// returns all recipes matching a category and value
+// ex. /recipes/meal/dinner
 server.get('/recipes/:category/:value', function(req,res) {
   Recipe.findByCategory(req.params.category,req.params.value, function(err,recipe) {
     if(err)
